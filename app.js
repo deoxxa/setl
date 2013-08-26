bd6007caba44bc51a9a24eb81aed0631b6c1d133
deoxxa <deoxxa@fknsrs.biz>
2013-08-27 01:55:25 +1000

    make renderer simpler, switch to layers

--- app.js
+++ app.js
@@ -1,42 +1,18 @@
 #!/usr/bin/env node
 
-var keypress = require("keypress");
-
 var Screen = require("./lib/screen"),
     World = require("./lib/world");
 
-keypress(process.stdin);
+var worldLayer = World();
 
 var screen = new Screen({
-  world: World(),
+  layers: [
+    worldLayer,
+  ],
 });
 
 screen.pipe(process.stdout);
 
-screen.on("cursor", screen.render.bind(screen));
-screen.on("scroll", screen.render.bind(screen));
+screen.render(0, 0);
 
-process.stdin.on("keypress", function(char, key) {
-  if (key && key.ctrl && key.name === "c") {
-    screen.reset();
-    return process.stdin.pause();
-  }
-
-  var x = 0, y = 0;
-
-  switch (key.name) {
-    case "left":  x = -1; break;
-    case "right": x =  1; break;
-    case "down":  y = -1; break;
-    case "up":    y =  1; break;
-  }
-
-  if (key.shift) {
-    screen.scrollBy(x, y);
-  } else {
-    screen.cursorBy(x, y);
-  }
-});
-
-process.stdin.setRawMode(true);
 process.stdin.resume();
