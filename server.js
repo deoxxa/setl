4c40f958706facf2c0eb6d5252cb85047d6af874
Tim <secoif@gmail.com>
2013-08-27 21:14:42 +1000

    Add dudeman.

--- server.js
+++ server.js
@@ -4,20 +4,26 @@ var net = require("net"),
     keypress = require("keypress");
 
 var World = require("./lib/world"),
-    Screen = require("./lib/screen");
+    Screen = require("./lib/screen"),
+    Cursor = require("./lib/cursor");
 
 var players = {};
 
 var world = new World();
 
 var server = net.createServer(function(socket) {
+  var cursor = new Cursor()
   var screen = new Screen({
     layers: [
       world.landscape,
+      world.actors,
+      cursor.data
     ],
   })
 
-  screen.pipe(socket)
+  screen
+  .pipe(socket)
+  .pipe(cursor.input)
 
   var x = 0, y = 0;
 
@@ -32,12 +38,12 @@ var server = net.createServer(function(socket) {
       return;
     }
 
-    switch (key.name) {
-      case "left":  x--; break;
-      case "right": x++; break;
-      case "up":    y--; break;
-      case "down":  y++; break;
-    }
+    //switch (key.name) {
+      //case "left":  x--; break;
+      //case "right": x++; break;
+      //case "up":    y--; break;
+      //case "down":  y++; break;
+    //}
 
     if (key.name === "escape") {
       socket.end();
